197f9c8555b580f9dc530f4b7923c64759ccb12f
Chanidapaka <chanidapapanta@gmail.com>
2024-10-27 22:20:35 +0700

    AddFile AppController02

--- src/main/java/sit/int202/demo/controllers/AppController.java
+++ src/main/java/sit/int202/demo/controllers/AppController.java
@@ -3,6 +3,7 @@ package sit.int202.demo.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+//สร้างอันนี้อันดับแรก
 @Controller
 public class AppController {
     //เมื่อเรียกด้วย/home ก็จะเข้าตรงนี้
