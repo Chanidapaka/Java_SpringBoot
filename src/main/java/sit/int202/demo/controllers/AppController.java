9ef196afbf796be148d8606fabe37d6b571631f6
INT202 <INT202@Train10-3-PC243.control.ad.sit.kmutt.ac.th>
2024-11-05 15:17:31 +0700

    Update AppController01

--- src/main/java/sit/int202/demo/controllers/AppController.java
+++ src/main/java/sit/int202/demo/controllers/AppController.java
@@ -1,7 +1,14 @@
 package sit.int202.demo.controllers;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 //สร้างอันนี้อันดับแรก
 @Controller
@@ -11,4 +18,23 @@ public class AppController {
     public String homePage(){
         return "home";
     };
+
+    @GetMapping("/theme")
+    public String selectTheme(){
+        return "theme";
+    };
+
+    @PostMapping("/theme")
+    public String applyTheme(@RequestParam String theme, HttpServletResponse response){
+        Cookie cookie = new Cookie("theme", theme);
+        cookie.setMaxAge(60*60*8);
+        response.addCookie(cookie);
+        return "home";
+
+        cookie  = new Cookie("fg", theme.equalsIgnoreCase("cadetblue")?"yeellow":"black");
+        cookie.setMaxAge(60*60*8);
+        response.addCookie(cookie);
+        return "home";
+    };
+
 }
--- src/main/java/sit/int202/demo/controllers/OfficeController.java
+++ src/main/java/sit/int202/demo/controllers/OfficeController.java
@@ -1,4 +1,5 @@
 package sit.int202.demo.controllers;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,24 +9,36 @@ import org.springframework.web.bind.annotation.RequestParam;
 import sit.int202.demo.entities.Office;
 import sit.int202.demo.repositories.OfficeRepository;
 import sit.int202.demo.services.OfficeService;
+
 import java.util.List;
-//@Controller@RequestMapping("/offices") //รับรีเควสที่เข้ามาใน officespublic class OfficeController {
-//    private OfficeRepository repository;
-//    private OfficeService service;
-//    @GetMapping("/all") //รับเฉพาะ get
-//    public String allOffices(Model model) {
-//    List<Office> officeList = repository.findAll();
-//    model.addAttribute("offices", officeList);
-//    return "offices_list";    }
-//    @GetMapping("/delete") // เมธอดที่จะรับคำขอ GET ที่ /offices
-//    public String deleteOfficeById(@RequestParam String officeCode, Model model) {
-// ดึงข้อมูลออฟฟิศตาม officeCode        Office office = service.deleteOffice(officeCode);
-// model.addAttribute("office", office);
-// model.addAttribute("message", office.getOfficeCode());
-// return "office_details";    }
-// @GetMapping("") // เมธอดที่จะรับคำขอ GET ที่ /offices
-// public String getOfficeById(@RequestParam String officeCode, Model model) {
-// ดึงข้อมูลออฟฟิศตาม officeCode
-// Office office = service.getOffice(officeCode);
-// model.addAttribute("office", office);
-// return "office_details";    }}
+
+@Controller
+@RequestMapping("/offices") //รับรีเควสที่เข้ามาใน offices
+public class OfficeController {
+    private OfficeRepository repository;
+    private OfficeService service;
+
+    @GetMapping("/all") //รับเฉพาะ get
+    public String allOffices(Model model) {
+        List<Office> officeList = repository.findAll();
+        model.addAttribute("offices", officeList);
+        return "office_list";
+    }
+
+    @GetMapping("/delete") // เมธอดที่จะรับคำขอ GET ที่ /offices
+    public String deleteOfficeById(@RequestParam String officeCode, Model model) {
+        // ดึงข้อมูลออฟฟิศตาม officeCode
+        Office office = service.deleteOffice(officeCode);
+        model.addAttribute("office", office);
+        model.addAttribute("message", office.getOfficeCode());
+        return "office_detail";
+    }
+
+    @GetMapping("") // เมธอดที่จะรับคำขอ GET ที่ /offices
+    public String getOfficeById(@RequestParam String officeCode, Model model) {
+        // ดึงข้อมูลออฟฟิศตาม officeCode
+        Office office = service.getOfficeByCode(officeCode);
+        model.addAttribute("office", office);
+        return "office_detail";
+    }
+}
